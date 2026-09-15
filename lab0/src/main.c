35cb368d26e527fddef19ad5d2be4afd53a4de38
dbosket <dbosket@engineering.upenn.edu>
2026-09-14 21:36:58 -0400

    Changes for 5-1

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -9,16 +9,18 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS   300
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+//#define LED0_NODE DT_ALIAS(led0)
+#define LED5180_NODE DT_ALIAS(led5180)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+//static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED5180_NODE, gpios);
 
 int main(void)
 {
